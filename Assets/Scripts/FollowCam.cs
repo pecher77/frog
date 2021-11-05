30800a004e17a9cedc1bbfd34d1ff5f4f54823a5
pecher77 <pecherikinad@gmail.com>
2021-11-05 19:18:51 +0200

    parrallax BG

--- Assets/Scripts/FollowCam.cs
+++ Assets/Scripts/FollowCam.cs
@@ -7,6 +7,8 @@ public class FollowCam : MonoBehaviour
     public Transform player;
     public float smoothTime = 0.2f;
     private Vector3 _velocity = Vector3.zero;
+    public float offsetX = 5.0f;
+    public float offsetY = 5.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,7 +18,7 @@ public class FollowCam : MonoBehaviour
     // Update is called once per frame
     void LateUpdate()
     {
-        Vector3 playerPosition = new Vector3(player.position.x + 5, player.position.y, transform.position.z);
+        Vector3 playerPosition = new Vector3(player.position.x + offsetX, player.position.y + offsetY, transform.position.z);
 
         transform.position = Vector3.SmoothDamp(transform.position, playerPosition, ref _velocity, smoothTime);
     }
